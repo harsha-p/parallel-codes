ca2be1842dd48715749274d33f8906afe259f1ab
harsha-p <harsha.pathuri@students.iiit.ac.in>
2021-06-23 14:09:32 +0530

    parallel code gives correct output

--- parallel/parallel.cpp
+++ parallel/parallel.cpp
@@ -1,125 +1,113 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <cstdio>
 #include <omp.h>
 #include "2DArray.h"
 #define MM 1000
 #define NN 1000
 #define PP 1000
 
-double **transpose(double **B, int pf, int pl, int nf, int nl)
-{
-    double **T = Allocate2DArray<double>((nl - nf), (pl - pf));
-#pragma omp parallel for
-    for (int i = 0; i < (nl - nf); i++)
-    {
-        for (int j = 0; j < (pl - pf); j++)
-        {
-            T[i][j] = B[j + pf][i + nf];
-        }
-    }
-    return T;
-}
+int min(int a, int b) { return a < b ? a : b; }
 
-void matmultleaf(int mf, int ml, int nf, int nl, int pf, int pl, double **A, double **B, double **C)
+void matmultleaf(int mf, int ml, int nf, int nl, int pf, int pl, double *A, double *B, double *C, int M, int N, int P)
 {
 #pragma omp parallel for
     for (int i = mf; i < ml; i++)
         for (int j = nf; j < nl; j++)
         {
-            // double * a = A[i];
-            // double * b = B[j];
-            double sum=0;
-            C[i][j]=0;
-            #pragma omp simd reduction(+:sum)
+            double sum = 0;
+#pragma omp simd reduction(+ \
+                           : sum)
             for (int k = pf; k < pl; k++)
             {
-                // sum += a[k] * b[k]; //using B transpose
-                sum += A[i][k]*B[j][k];
+                // sum += A[i * P + j] * B[j * P + k];
+                sum += A[i * P + k] * B[k * N + j];
             }
-            C[i][j] += sum; 
+            C[i * N + j] = sum;
         }
 }
 
 int main(int argc, char *argv[])
 {
-    int i, j,LOOP_COUNT=10;
+    int i, j, LOOP_COUNT = 10;
     double start, time1, time2;
 
     int M = MM;
     int N = NN;
     int P = PP;
 
-    if(argc >= 4){ 
-	    M = atoi(argv[1]);
-	    P = atoi(argv[2]);
-	    N = atoi(argv[3]);
-    }
-    if(argc==5) LOOP_COUNT = atoi(argv[4]);
-    double **A = Allocate2DArray<double>(M, P);
-    // double **B = Allocate2DArray<double>(P, N);
-    double **BT = Allocate2DArray<double>(N, P);
-
-    double **C = Allocate2DArray<double>(M, N);
-
-    for (i = 0; i < M; i++)
+    if (argc >= 4)
     {
-        for (j = 0; j < P; j++)
-        {
-            A[i][j] = (i * P + j + 1);
-        }
+        M = atoi(argv[1]);
+        P = atoi(argv[2]);
+        N = atoi(argv[3]);
     }
+    if (argc == 5)
+        LOOP_COUNT = atoi(argv[4]);
 
-    // for (i = 0; i < P; i++)
-    // {
-    //     for (j = 0; j < N; j++)
-    //     {
-    //         B[i][j] = (-i * N - j - 1);
-    //     }
-    // }
+    double *A = (double *)malloc(M * P * sizeof(double));
+    double *B = (double *)malloc(P * N * sizeof(double));
+    double *C = (double *)malloc(M * N * sizeof(double));
 
-    for (i = 0; i < N; i++)
-    {
-        for (j = 0; j < P; j++)
-        {
-            BT[i][j] = (-i * P - j - 1);
-        }
-    }
-
-    for (i = 0; i < M; i++)
-    {
-        for (j = 0; j < N; j++)
-        {
-            C[i][j] = 0.0 ;
-        }
-    }
+    for (i = 0; i < M * P; i++)
+        A[i] = (double)(i + 1);
+    for (i = 0; i < P * N; i++)
+        B[i] = (double)(-i - 1);
+    for (i = 0; i < M * N; i++)
+        C[i] = 0.0;
 
     printf("Matrix Dimensions: M = %d  P = %d  N = %d\n\n", M, P, N);
 
     printf("Execute matmult parallel\n");
-    matmultleaf(0,M,0,N,0,P,A,BT,C);// using B transpose
+    matmultleaf(0, M, 0, N, 0, P, A, B, C, M, N, P); // using B transpose
     start = omp_get_wtime();
-    for(i=0;i<LOOP_COUNT;i++){
-        matmultleaf(0,M,0,N,0,P,A,BT,C);// using B transpose
+    for (i = 0; i < LOOP_COUNT; i++)
+    {
+        matmultleaf(0, M, 0, N, 0, P, A, B, C, M, N, P); // using B transpose
     }
-    time1 = (omp_get_wtime() - start)/LOOP_COUNT;
-    printf("Time = %.5f milli seconds\n\n", time1*1000);
+    time1 = (omp_get_wtime() - start) / LOOP_COUNT;
+    printf("Time = %.5f milli seconds\n\n", time1 * 1000);
 
-    Free2DArray<double>(A);
-    // Free2DArray<double>(B);
-    Free2DArray<double>(BT);
-    Free2DArray<double>(C);
+    for (int i = 0; i < min(6, M); i++)
+    {
+        for (int j = 0; j < min(6, P); j++)
+        {
+            printf("%12.5G", A[i * P + j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for (int i = 0; i < min(6, P); i++)
+    {
+        for (int j = 0; j < min(6, N); j++)
+        {
+            printf("%12.5G", B[i * N + j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for (int i = 0; i < min(6, M); i++)
+    {
+        for (int j = 0; j < min(6, N); j++)
+        {
+            printf("%12.5G", C[i * N + j]);
+        }
+        printf("\n");
+    }
+    free(A);
+    free(B);
+    free(C);
 
-  if (time1 < 0.9 / LOOP_COUNT)
-  {
-    time1 = 1.0 / LOOP_COUNT / time1;
-    i = (int)(time1* LOOP_COUNT) + 1;
-    printf(" It is highly recommended to define LOOP_COUNT for this example on "
-           "your \n"
-           " computer as %i to have total execution time about 1 second for "
-           "reliability \n"
-           " of measurements\n\n",
-           i);
-  }
+    if (time1 < 0.9 / LOOP_COUNT)
+    {
+        time1 = 1.0 / LOOP_COUNT / time1;
+        i = (int)(time1 * LOOP_COUNT) + 1;
+        printf(" It is highly recommended to define LOOP_COUNT for this example on "
+               "your \n"
+               " computer as %i to have total execution time about 1 second for "
+               "reliability \n"
+               " of measurements\n\n",
+               i);
+    }
 
     return 0;
 }
--- serial/mat_mul.cpp
+++ serial/mat_mul.cpp
@@ -2,10 +2,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void mat_mul(double *A, double *B, double *C, int m, int n, int p) {
+void mat_mul(double *A, double *B, double *C, int m, int n, int p)
+{
   double sum;
-  for (int i = 0; i < m; i++) {
-    for (int j = 0; j < n; j++) {
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
       sum = 0.0;
       for (int k = 0; k < p; k++)
         C[n * i + j] += A[p * i + k] * B[n * k + j];
@@ -13,13 +16,15 @@ void mat_mul(double *A, double *B, double *C, int m, int n, int p) {
   }
 }
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
   // A m*p, B p*n, C m*n
   int m, n, p, i;
   m = 1000;
   p = 1000;
   n = 1000;
-  if(argc == 4){
+  if (argc == 4)
+  {
     m = atoi(argv[1]);
     p = atoi(argv[2]);
     n = atoi(argv[3]);
@@ -28,28 +33,52 @@ int main(int argc, char const *argv[]) {
   A = (double *)malloc(m * p * sizeof(double));
   B = (double *)malloc(p * n * sizeof(double));
   C = (double *)malloc(m * n * sizeof(double));
-  if (A == NULL || B == NULL || C == NULL) {
+  if (A == NULL || B == NULL || C == NULL)
+  {
     cout << "Error allocating memory for matrices\n";
     free(A);
     free(B);
     free(C);
     return 0;
   }
-  for (i = 0; i < (m * p); i++) {
+  for (i = 0; i < (m * p); i++)
+  {
     A[i] = (double)(i + 1);
   }
-  for (i = 0; i < (p * n); i++) {
+  for (i = 0; i < (p * n); i++)
+  {
     B[i] = (double)(-i - 1);
   }
-  for (i = 0; i < (m * n); i++) {
+  for (i = 0; i < (m * n); i++)
+  {
     C[i] = 0.0;
   }
   clock_t start_time = clock();
   mat_mul(A, B, C, m, n, p);
   printf("time taken %f\n", (double)(clock() - start_time) / CLOCKS_PER_SEC);
-  for (int i = 0; i < 6; i++) {
-    for (int j = 0; j < 6; j++) {
-      printf("%12.5G", C[j + i * n]);
+  for (int i = 0; i < min(6, m); i++)
+  {
+    for (int j = 0; j < min(6, p); j++)
+    {
+      printf("%12.5G", A[i * p + j]);
+    }
+    printf("\n");
+  }
+  printf("\n");
+  for (int i = 0; i < min(6, p); i++)
+  {
+    for (int j = 0; j < min(6, n); j++)
+    {
+      printf("%12.5G", B[i * n + j]);
+    }
+    printf("\n");
+  }
+  printf("\n");
+  for (int i = 0; i < min(6, m); i++)
+  {
+    for (int j = 0; j < min(6, n); j++)
+    {
+      printf("%12.5G", C[i * n + j]);
     }
     printf("\n");
   }
